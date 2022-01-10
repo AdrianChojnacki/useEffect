6b8e5b5545bfd9de999f97d9a1b5a3aef338c1ac
Michał Zieliński (dev) <michal.zielinski@crafton.pl>
2022-01-10 19:46:57 +0100

    first use

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
   const [counter, setCounter] = useState(0);
 
   const handler = () => setCounter(prevValue => prevValue + 1);
+
+  useEffect(() => {
+    alert('Hello');
+  });
 
   return (
     <div onClick={handler}>
