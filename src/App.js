cd20a46d518e10b64ce3d82ef4b7025479298234
Michał Zieliński (dev) <michal.zielinski@crafton.pl>
2022-01-10 20:02:44 +0100

    added effect

--- src/App.js
+++ src/App.js
@@ -3,19 +3,40 @@ import './App.css';
 
 const App = () => {
   const [counter, setCounter] = useState(0);
+  const [isActive, setIsActive] = useState(true);
 
   const handler = () => setCounter(prevValue => prevValue + 1);
+  const toggleVisibilityCounter = () => setIsActive(prevValue => !prevValue);
 
-  useEffect(() => {
-    alert('Hello');
-  });
+  // useEffect(() => {
+  //   alert('Hello')
+  // }, []);
 
   return (
-    <div onClick={handler}>
-      <p>Licznik</p>
-      <p>{counter}</p>
+    <div>
+      <button onClick={toggleVisibilityCounter}>
+        Pokaż/ukryj komponent
+      </button>
+      {isActive && <Counter />}
     </div>
   );
+}
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  const handleMouseMove = event => setCounter(event.clientX);
+
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMouseMove);
+  }, []);
+
+  return (
+    <div>
+      <p>{counter}</p>
+      {/* <p>{rerenderCounter}</p> */}
+    </div>
+  )
 }
 
 export default App;
